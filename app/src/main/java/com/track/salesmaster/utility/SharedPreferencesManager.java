a1c257fffde92c0779f268d6a7ca5f9c5ec99ca0
Vicky Varma <vickyvarmaseo@gmail.com>
2020-02-07 23:45:12 +0530

    visit history added

--- app/src/main/java/com/track/salesmaster/utility/SharedPreferencesManager.java
+++ app/src/main/java/com/track/salesmaster/utility/SharedPreferencesManager.java
@@ -2,11 +2,22 @@ package com.track.salesmaster.utility;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.track.salesmaster.MainActivity;
 import com.track.salesmaster.response.Data;
 
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class SharedPreferencesManager {
+    private String TAG = "v-v "+ getClass().getName();
     private static final String SHARED_PREF_NAME = "my_shared_pref";
+    private static final String SHARED_PREF_ADDRESS = "my_address";
     private static SharedPreferencesManager mInstance;
     private Context mContext;
 
@@ -31,6 +42,28 @@ public class SharedPreferencesManager {
         editor.putString("mobile", data.getUserMobile());
 
         editor.apply();
+    }
+
+    //saving array list to shared preference
+    public void saveAddress(ArrayList arrayList) {
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(SHARED_PREF_ADDRESS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(arrayList);
+        editor.putString("LIST", json);
+        editor.apply();
+        Log.d(TAG, "save address");
+    }
+
+    //re-calling array list from shared preference
+    public ArrayList<String> getAddressList() {
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(SHARED_PREF_ADDRESS, Context.MODE_PRIVATE);
+        Gson gson = new GsonBuilder().excludeFieldsWithModifiers(Modifier.FINAL, Modifier.TRANSIENT, Modifier.STATIC)
+                .serializeNulls() // modified way to initializing gson because of this error "java.lang.SecurityException: Can't make a java.lang.reflect.Method constructor accessible"
+                .create();
+        String jsonData = sharedPreferences.getString("LIST", null);
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        return gson.fromJson(jsonData, type); // returning array list
     }
 
     public boolean isLoggedIn() {
